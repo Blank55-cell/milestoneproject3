077250f72f327ab7ac64790426b9769d63a284f7
Blank55 <carlosalba101@proton.me>
2026-03-09 16:16:29 +0000

    Added in the structure for my html and started work on my js will further refine the html structure and will start to add in the layout for the sign in page

---
+++ docs/assets/js/javascript.js
@@ -0,0 +1,5 @@
+// Simple panel switching for sign-in
+const loginPanel = document.getElementById("login-panel");
+const registerPanel = document.getElementById("registerPanel");
+const forgotPanel = document.getElementById("forgotPanel");
+const resetPanel = document.getElementById("resetPanel");
