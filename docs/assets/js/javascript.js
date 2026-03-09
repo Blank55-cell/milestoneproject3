29d1cbae20587d1b2ef19b29f2d6aec51fb1a057
Blank55 <carlosalba101@proton.me>
2026-03-09 16:22:43 +0000

    Forgot to save the js before committing

--- docs/assets/js/javascript.js
+++ docs/assets/js/javascript.js
@@ -3,3 +3,8 @@ const loginPanel = document.getElementById("login-panel");
 const registerPanel = document.getElementById("registerPanel");
 const forgotPanel = document.getElementById("forgotPanel");
 const resetPanel = document.getElementById("resetPanel");
+
+
+function show(panel) {
+    [loginPanel]
+}
