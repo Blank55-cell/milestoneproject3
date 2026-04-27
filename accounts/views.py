baa7e6b1ab4a2f1e5881f1e25e1fb1782bc1aaba
Blank55 <carlosalba101@proton.me>
2026-04-27 23:23:07 +0100

    started to build my accounts section to allow for signing up and registering

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth import get_user_model, authenticate, login, logout
+from django.http import JsonResponse
+from django.shortcuts import render
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
+
