d519ba0e1627cee01ca749f2f6ac34bb2421e0ba
Blank55 <carlosalba101@proton.me>
2026-05-01 00:45:55 +0100

    Updated my books.view to fix the 405 error turns out it was trying to fetch a page that didbnt exist from my templates ultimately showing an error 405 as it was trying to redirect me to a login.html but i had made a mistake as i wanted it to be redirected to my account.html where the login was actually located

--- books/views.py
+++ books/views.py
@@ -1,17 +1,58 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Book
 
 def home(request):
     return render(request, 'home.html', {'name': 'Home'})
 
+
+@login_required
 def add_book(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        author = request.POST.get('author')
+        favourite_chapter = request.POST.get('favourite_chapter')
+        notes = request.POST.get('notes')
+        status = request.POST.get('reading_status')
+
+        # Save the book with the logged-in user
+        Book.objects.create(
+            user=request.user,
+            title=title,
+            author=author,
+            favourite_chapter=favourite_chapter,
+            notes=notes,
+            status=status
+        )
+
+        return redirect('library')
+
     return render(request, 'add_book.html', {'name': 'Add Book'})
 
+
+@login_required
 def library(request):
-    return render(request, 'library.html', {'name': 'Library'})
+    # Only show books belonging to the logged-in user
+    books = Book.objects.filter(user=request.user)
+    return render(request, 'library.html', {'name': 'Library', 'books': books})
 
+
+@login_required
 def search(request):
-    return render(request, 'search.html', {'name': 'Search'})
+    query = request.GET.get('q', '')
 
+    if query:
+        books = Book.objects.filter(user=request.user, title__icontains=query)
+    else:
+        books = []
+
+    return render(request, 'search.html', {
+        'name': 'Search',
+        'books': books,
+        'query': query
+    })
+
+
+@login_required
 def account(request):
     return render(request, 'account.html', {'name': 'Account'})
