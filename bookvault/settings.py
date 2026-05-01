2959615f1f35ef4f4731f47eb12718d08740506b
Blank55 <carlosalba101@proton.me>
2026-05-01 16:59:31 +0100

    Added in static files to help load css and js hopefully works now

--- bookvault/settings.py
+++ bookvault/settings.py
@@ -127,9 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Change this line
@@ -138,3 +137,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+WHITENOISE_IGNORE_MISSING_FILES = True
