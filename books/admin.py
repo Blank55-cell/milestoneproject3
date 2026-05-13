c9d966c49bd7f34a3d47672fc981937f807dbffc
Blank55 <carlosalba101@proton.me>
2026-05-13 01:13:53 +0100

    Added in the admin py to help me manage the database

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Book
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    """
+    Configuration for the Book model in the admin panel.
+    """
+    list_display = ('title', 'author', 'genre')  # Columns shown in the list view
+    search_fields = ('title', 'author')         # Adds a search bar to the admin
+    list_filter = ('genre',)                    # Adds a filter sidebar
