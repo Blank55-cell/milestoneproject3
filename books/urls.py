41f5826f0808d36dfaec072df1213dfd105201eb
Carlos <carlosalba101@proton.me>
2026-05-22 12:01:39 +0100

    deleted account path in books

--- books/urls.py
+++ books/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('add_book/', views.add_book, name='add_book'),
     path('library/', views.library, name='library'),
     path('search/', views.search, name='search'),
-    path('account/', views.account, name='account'),
     path('delete/<int:book_id>/', views.delete_book, name='delete_book'),
     path('book/<int:book_id>/', views.book_details, name='book_details'),
     path('edit/<int:book_id>/', views.edit_book, name='edit_book'),
